69ffbcb885cf49eae8d5caa44f790ad9dbd5e647
Robert Susewell <susewell@live.unc.edu>
2022-04-18 23:55:51 -0400

    try

--- database.js
+++ database.js
@@ -2,13 +2,13 @@ const database = require('better-sqlite3')
 
 const logdb = new database('log.db')
 
-const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and 'access';`)
+const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
 let row = stmt.get();
 if (row === undefined) {
     console.log('Log database appears to be empty. Creating log database.....')
     
     const sqlInit = `
-        CREATE TABLE access ( id INTEGER PRIMARY KEY, remote-addr VARCHAR, remote-user VARCHAR, datetime VARCHAR, method VARCHAR, url VARCHAR, url VARCHAR, http-version NUMERIC, status INTEGER, content-length NUMERIC)
+        CREATE TABLE access ( id INTEGER PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time TEXT, method TEXT, url TEXT, httpversion TEXT, status TEXT, referrer TEXT, useragent TEXT)
     `
     logdb.exec(sqlInit)
 } else {
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const errorhandler = require('errorhandler')
 const fs = require('fs')
 
 const args = require ('minimist')(process.argv.slice(2));
+
 args['port'];
 const port_args = args.port;
 
@@ -22,16 +23,74 @@ const logging = (req, res, next) => {
 
 
 
-var port = 5000
+var port = args.port || args.p || 5000
 
-if (port_args != null) {
-    var port = port_args;
-}
+
 
 const server = app.listen(port, () => {
     console.log('App is running on a port %PORT%'.replace('%PORT%', port))
 })
 
+app.get("/app/", (req, res, next) =>{
+    res.json({"message" : "Your API works! (200)"});
+    res.status(200);
+});
+
+if (args.log == 'false') {
+    console.log("You are not creating file access.log")
+} else {
+    const accesslog = fs.createWriteStream('access.log', {flags: 'a'})
+    app.use(morgan('combined', {stream: accessLog}))
+}
+
+const help = (`
+server.js [options]
+  --port		Set the port number for the server to listen on. Must be an integer
+              	between 1 and 65535.
+
+  --debug	If set to true, creates endlpoints /app/log/access/ which returns
+              	a JSON access log from the database and /app/error which throws 
+              	an error with the message "Error test successful." Defaults to 
+		false.
+
+  --log		If set to false, no log files are written. Defaults to true.
+		Logs are always written to database.
+
+  --help	Return this message and exit.
+`)
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
+
+app.use((req, res, next) => {
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    }
+    console.log(logdata)
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+    next();
+});
+
+if (args.debug || args.d) {
+    app.get('/app/log/access/', (req, res, next) => {
+        const stmt = db.prepare('SELECT * FROM accesslog').all()
+        res.status(200).json (stmt)
+    })
+    app.get ('/app/error/', (req, res, next) => {
+        throw new Error('Error')
+    })
+}
 function coinFlip() {
     let flip =  Math.random();
     if (flip < 0.5){
